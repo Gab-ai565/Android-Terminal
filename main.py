c14b29cda90baf3be047c7f45b745f5a2e0e2c0a
Angel <angelogabrieldesalesguerra@gmail.com>
2026-01-15 19:46:27 -0300

    Add ls and pwd commands

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 import os
 
+def ls():
+    print(os.listdir())
+
+def pwd():
+    print(os.getcwd())
+
+commands = {
+    "ls": ls,
+    "pwd": pwd
+}
+
 while True:
     cmd = input(">> ")
-    
+
     if cmd == "quit":
-        exit()
+        print("Leaving... ")
+        break
+    elif cmd in commands:
+        commands[cmd]()
+    else:
+        print("Error: Command Does Not Exist")
